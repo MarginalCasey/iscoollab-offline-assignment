21e8fc1ea4eaa0d32d4b228e2d2ddad0acb67aac
張文源 <c29429768@gmail.com>
2024-03-21 16:33:06 +0800

    Refactor: separating parsing logic into parseCategoryListData

--- src/providers/fetchMenu/index.ts
+++ src/providers/fetchMenu/index.ts
@@ -13,29 +13,33 @@ interface fetchMenuResponse {
   category_list_json: JsonType<"product_id" | "sort" | "name" | "product_list">;
 }
 
+function parseCategoryListData(data: fetchMenuResponse["category_list_json"]) {
+  const { schema, data: categoryData } = data;
+  const categoryList = Object.values(categoryData)
+    .sort((a, b) => {
+      const orderA = a[schema.sort] as number;
+      const orderB = b[schema.sort] as number;
+      return orderA - orderB;
+    })
+    .map((category) => {
+      return {
+        id: category[schema.product_id],
+        name: category[schema.name],
+        productList: category[schema.product_list],
+      } as Category;
+    });
+
+  return categoryList;
+}
+
 function fetchMenu(): Promise<Menu> {
   return fetch("/menu.json")
     .then<fetchMenuResponse>((response) => response.json())
     .then<Menu>((data) => {
       console.log(data);
 
-      const { schema, data: categoryData } = data.category_list_json;
-      const categoryList = Object.values(categoryData)
-        .sort((a, b) => {
-          const orderA = a[schema.sort] as number;
-          const orderB = b[schema.sort] as number;
-          return orderA - orderB;
-        })
-        .map((category) => {
-          return {
-            id: category[schema.product_id],
-            name: category[schema.name],
-            productList: category[schema.product_list],
-          } as Category;
-        });
-
       return {
-        categoryList,
+        categoryList: parseCategoryListData(data.category_list_json),
       };
     });
 }
